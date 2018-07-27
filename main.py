38e0556036b0c58ccf8e629196c1f5d25836711a
Tobi <liupenghui_best@163.com>
2018-07-27 23:28:43 +0800

    简单获取网页源代码功能

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-import urllib
+import urllib.request
+import ssl
+
+context = ssl._create_unverified_context()
+response = urllib.request.urlopen('https://www.python.org', context=context)
+print(response.read().decode('utf-8'))
+print("***************")
+print(type(response))
