5ed5ce2001e57ca9b051f6b17c7198df8b9ba19f
Tobi <liupenghui_best@163.com>
2018-07-28 15:41:22 +0800

    简单构造请求头

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 import urllib.request
+import urllib.parse
 import ssl
+import urllib.error
+import socket
+try:
+    headers = {'User-Agent': 'Mozilla / 5.0(Macintosh;Intel Mac OS X) AppleWebKit / 537.36(KHTML, like Gecko)',
+               'Host': 'httpbin.org'}
+    dict = {'name':'Tobi'}
+    request = urllib.request.Request('http://httpbin.org/post', headers=headers, data=bytes(urllib.parse.urlencode(dict),encoding='utf8'), method='POST')
+    print("***************")
+    response = urllib.request.urlopen(request, context=ssl._create_unverified_context())
+    if  response.status != 200:
+        print('status : ' + response.status)
+    print("*******result********")
+    print(response.read().decode('utf-8'))
+    print(type(response))
+    print(response.status)
+    print(response.getheaders())
+    print(response.getheader('Server'))
 
-context = ssl._create_unverified_context()
-response = urllib.request.urlopen('https://www.python.org', context=context)
-print(response.read().decode('utf-8'))
-print("***************")
-print(type(response))
+except urllib.error.URLError as e:
+    if  isinstance(e.reason, socket.timeout):
+        print('time out!!!')
+
+    print('error ' + e.reason)
