cc0f7b6eef067ded78bb34351f4a3c78b6a085db
Tobi <liupenghui_best@163.com>
2018-07-31 00:18:09 +0800

    新包requests功能测试

--- test/cookie_test.py
+++
@@ -1,9 +0,0 @@
-from urllib.robotparser import RobotFileParser
-from urllib.request import urlopen
-
-def check_robot_txt(url):
-    rp = RobotFileParser(url+ '/robots.txt')
-    rp.read()
-  #  rp.parse(urlopen(url.read().decode('utf-8').split('\n')))
-    print(rp.can_fetch('*', url + '/stock/'))
-    return
