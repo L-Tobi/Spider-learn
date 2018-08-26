89251edbe4f0987df11931b8335ab844e379f606
Tobi <liupenghui_best@163.com>
2018-08-27 01:04:56 +0800

    尝试将数据输出到txt但是遇到一点BUG

--- Practice/get_movies_rank.py
+++ Practice/get_movies_rank.py
@@ -4,4 +4,6 @@ def get_one_page(url):
     responses = requests.get(url)
     if  responses.status_code == 200:
         return responses.text
+    else:
+        print('error : ' ,responses.status_code)
     return None
--- main.py
+++ main.py
@@ -7,30 +7,42 @@ from test import beautifulsoup_test
 from  test import lxml_test
 import sys
 import re
+import time
 from time import sleep
 #urllib_test.check_robot_txt('https://www.sina.com.cn')
 #requests_test.requests_test()
 #regex_test.practice_regex()
 #url = 'http://maoyan.com/board/4'
 #url = 'http://finance.sina.com.cn/realstock/company/sz002202/nc.shtml'
+
+file = open('002202.txt','w+')
 while(True):
-    url = 'https://hq.sinajs.cn/?rn=1534081330022&list=sz002202,sz002202_i'
-    html = get_movies_rank.get_one_page(url)
-    #print(html)
+    try:
+        url = 'https://hq.sinajs.cn/?rn=1534081330022&list=sz002202,sz002202_i'
+        html = get_movies_rank.get_one_page(url)
+      #  print(html)
+        if html == None:
+            sleep(1)
+            continue
+        #price = re.match('^var.*,\d.*|', html)
+        price = re.search(',.*?,.*?,.*?,.*?,.*?,.*?,.*?,' ,html,re.S)
+        result = price.group(0)
+        #print(result)
+        result =  result.split(',')
+        count = file.write(time.asctime(time.localtime(time.time())))
+        print(count)
+        print('current :', result[3], end=' ', flush=True)
+        print('open :', result[1],end=' ', flush = True)
+        print('yesterday :', result[2],end=' ', flush = True)
+        print('highest :', result[4],end=' ', flush = True)
+        print('lowest :', result[5],end=' ', flush = True)
+        print('time :', time.asctime(time.localtime(time.time())), end='\n', flush=True)
+        sys.stdout.flush()
+        sleep(5)
+    except:
+        print('something is wrong')
 
-    #price = re.match('^var.*,\d.*|', html)
-    price = re.search(',.*?,.*?,.*?,.*?,.*?,.*?,.*?,' ,html,re.S)
-    result = price.group(0)
-    #print(result)
-    result =  result.split(',')
-
-    print('open :', result[1],end='', flush = True)
-    print('yesterday :', result[2],end='', flush = True)
-    print('current :', result[3],end='', flush = True)
-    print('highest :', result[4],end='', flush = True)
-    print('lowest :', result[5],end='', flush = True)
-    sys.stdout.flush()
-    sleep(5)
+file.close()
 #lxml_test.practice_lxml_test(url)
 
 #beautifulsoup_test.practice_beautifulsoup()
