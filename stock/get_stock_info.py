00a62159bbf7d65cc67538183faf0cda292fcf3e
liupenghui <liupenghui_best@163.com>
2018-09-05 18:12:01 +0800

    准备扩大收集数据规模，并且提交今天收集的数据

--- stock/get_stock_info.py
+++ stock/get_stock_info.py
@@ -5,15 +5,16 @@ import csv
 def get_stock_start(url):
 
     #ready to merge this code
-    responses = requests.get('https://hq.sinajs.cn/rn=1535455880338&list=s_sh000001,s_sz399001,s_sh000300,s_sz399415,s_sz399006')
-    sh000001 = re.search('\上证指数,(.*?),(.*?),(.*?),(\d+),(\d+)?', responses.text, re.S)
-    sz399001 = re.search('\深证成指,(.*?),(.*?),(.*?),(\d+),(\d+)?' ,responses.text, re.S)
-    sz399415 = re.search('\沪深300,(.*?),(.*?),(.*?),(\d+),(\d+)?', responses.text, re.S)
-    sz399006 = re.search('\创业板指,(.*?),(.*?),(.*?),(\d+),(\d+)?', responses.text, re.S)
+    #responses = requests.get('https://hq.sinajs.cn/rn=1535455880338&list=s_sh000001,s_sz399001,s_sh000300,s_sz399415,s_sz399006')
 
-    code_list = 'https://hq.sinajs.cn/?rn=1534081330022&list=sz002202,sz300098,sz300284'
+
+    code_list = 'https://hq.sinajs.cn/?rn=1534081330022&list=s_sh000001,s_sz399001,s_sh000300,s_sz399415,s_sz399006,sz002202,sz300098,sz300284'
     results_list = requests.get(code_list)
-  #  print(results_list.text)
+    print(results_list.text)
+    sh000001 = re.search('\上证指数,(.*?),(.*?),(.*?),(\d+),(\d+)?', results_list.text, re.S)
+    sz399001 = re.search('\深证成指,(.*?),(.*?),(.*?),(\d+),(\d+)?' ,results_list.text, re.S)
+    sz399415 = re.search('\沪深300,(.*?),(.*?),(.*?),(\d+),(\d+)?', results_list.text, re.S)
+    sz399006 = re.search('\创业板指,(.*?),(.*?),(.*?),(\d+),(\d+)?', results_list.text, re.S)
     sz002202 = re.search('\金风科技,(.*?),(.*?),(.*?),(.*?),(.*?),(.*?),(.*?),(.*?),(.*?),(.*?).*,(.*?),(.*?),(.*?),.*?"', results_list.text, re.S)
     sz300098 = re.search('\高新兴,(.*?),(.*?),(.*?),(.*?),(.*?),(.*?),(.*?),(.*?),(.*?),(.*?)?', results_list.text,re.S)
     sz300284 = re.search('\苏交科,(.*?),(.*?),(.*?),(.*?),(.*?),(.*?),(.*?),(.*?),(.*?),(.*?)?', results_list.text, re.S)
@@ -34,14 +35,24 @@ def get_stock_start(url):
     print('=====================================================================')
     pm_start_time = time.strftime("%Y-%m-%d ", time.localtime()) + '12:57:00'
     pm_end_time = time.strftime("%Y-%m-%d ", time.localtime()) + '15:03:00'
-    am_start_time = time.strftime("%Y-%m-%d ", time.localtime()) + '09:13:00'
+    am_start_time = time.strftime("%Y-%m-%d ", time.localtime()) + '09:23:00'
     am_end_time = time.strftime("%Y-%m-%d ", time.localtime()) + '11:33:00'
     current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 
     if (current_time > am_start_time and current_time < am_end_time ) or ( current_time > pm_start_time and current_time < pm_end_time):
         #for reduce storage , data should be optimized that cut the same as adjacent data.
    # if(True):
+        #while item in code_list:
 
+        with open('data/' + stock_code + ' ' + time.strftime("%Y-%m-%d", time.localtime()) + '.csv', 'a',newline='') as csvfile:
+            fieldnames = ['price', 'open', 'yesterday', 'highest', 'lowest', 'volumn', 'money', 'turnover', 'time']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writerow({'price': stock_code.group(3), 'open': stock_code.group(1), 'yesterday': stock_code.group(2),
+                             'highest': stock_code.group(4),
+                             'lowest': stock_code.group(5), 'volumn': stock_code.group(8), 'money': stock_code.group(9),
+                             'turnover': '{0:.5f}'.format(float(stock_code.group(8)) / 28548070.23),
+                             'time': info_time})
+"""
         with open('data/002202' + ' ' + time.strftime("%Y-%m-%d", time.localtime()) + '.csv', 'a',newline='') as csvfile:
             fieldnames = ['price','open','yesterday','highest','lowest','volumn','money','turnover', 'time']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
@@ -82,7 +93,7 @@ def get_stock_start(url):
             fieldnames = ['price', 'change', 'increase', 'volumn', 'money', 'time']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writerow({'price': sz399006.group(1), 'change': sz399006.group(2),'increase' : sz399006.group(3),'volumn' : sz399006.group(4), 'money': sz399006.group(5), 'time': info_time})
-
+"""
         # https://hq.sinajs.cn/rn=1535371925672&list=s_sh000001,s_sz399001,CFF_RE_IC0,rt_hkHSI,gb_$dji,gb_ixic,b_SX5E,b_UKX,b_NKY,hf_CL,hf_GC,hf_SI,hf_CAD
         # http://vip.stock.finance.sina.com.cn/quotes_service/view/CN_TransListV2.php?num=11&symbol=sz002202&rn=25589834
         # https://hq.sinajs.cn/rn=1535390045016&list=s_sh000001,s_sz399001,CFF_RE_IC0,rt_hkHSI,gb_$dji,gb_ixic,b_SX5E,b_UKX,b_NKY,hf_CL,hf_GC,hf_SI,hf_CAD
