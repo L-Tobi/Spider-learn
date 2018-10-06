d88e9a034ada7b0cba4973027161cf8f321b3502
Tobi <liupenghui_best@163.com>
2018-10-07 03:48:08 +0800

    删除老文件夹，并且抓取到汇率信息，准备存入数据库

--- finance/get_exchange_rate_info.py
+++ finance/get_exchange_rate_info.py
@@ -1,14 +1,32 @@
 import requests
 from bs4 import BeautifulSoup
 
+current_exchange_rate = 5
+current_exchange_rate_date = 6
+current_exchange_rate_seconds = 7
+
+
 def get_exchange_rate():
     url = 'http://www.boc.cn/sourcedb/whpj/'
     result = requests.get(url)
 
     soup = BeautifulSoup(result.text, 'lxml')
     print('===========')
-    for ul in soup.find_all(name='tr'):
-        print(ul.find_all(name='td'))
+    # print (soup.div.children)
+    # print (soup.select('tr'))
+    for i,th in enumerate( soup.div.find_all(name='tr')):
+        if i > 1 and i < 29:
+            try:
+                print (th.find_all(name='td')[current_exchange_rate])
+                print (th.find_all(name='td')[current_exchange_rate_date])
+                print (th.find_all(name='td')[current_exchange_rate_seconds])
+                print (i)
+            except Exception as e:
+                print (str(e), i)
+        print ('=============================')
+    # print (soup.find_all(attrs={'class':'odd'}))
+    # for ul in soup.find_all(name='tr'):
+    #     print(ul.find_all(name='td'))
 
 
 
