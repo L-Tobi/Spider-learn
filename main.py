66487096efa89a9b65aa4391943d53745bdc70a8
303841776 <303841776@qq.com>
2018-10-14 02:39:25 +0800

    大幅修改内部结构后（一半）版本

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from urllib.request import ProxyHandler , build_opener
-from finance import get_stock_info
-from finance import get_exchange_rate_info
+from finance import stock
+from finance import exchange_rate
 import sys
 import re
 import time
@@ -10,7 +10,7 @@ from datetime import datetime
 from time import sleep
 from test import proxy_test
 from database import mysql
-
+from tool import debug
 
 pm_start_time = time.strftime("%Y-%m-%d ", time.localtime()) + '13:00:00'
 pm_end_time = time.strftime("%Y-%m-%d ", time.localtime()) + '15:01:00'
@@ -24,21 +24,24 @@ collect_summary_data = True
 exchange_rate_time = datetime.now()
 
 def get_realtime_stock_info():
-    while(True):
-        current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-        if (current_time > am_start_time and current_time < am_end_time) or (current_time > pm_start_time and current_time < pm_end_time):
-            get_stock_info.get_stock_codes_info(current_time)
-            break
-        sleep(50)
+    china_stock = stock.china()
+    china_stock.get_stock_codes_info(current_time= debug.current_time())
+
+    # while(True):
+    #     current_time = debug.current_time()
+    #     if (current_time > am_start_time and current_time < am_end_time) or (current_time > pm_start_time and current_time < pm_end_time):
+    #         stock.china.get_stock_codes_info(current_time)
+    #     break
+    #     sleep(50)
 
 thread_realtime = threading.Thread(target=get_realtime_stock_info)
 thread_realtime.start()
 
 
 while(True):
-    current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    current_time = debug.current_time()
     if ((datetime.now() - exchange_rate_time).seconds > 300):
-        get_exchange_rate_info.get_exchange_rate()
+        exchange_rate.get_exchange_rate()
         exchange_rate_time = datetime.now()
     sleep(30)
     #get_stock_info.get_valid_stock_code('60')
--- tool/debug.py
+++ tool/debug.py
@@ -1,18 +1,20 @@
 import  time
 
-def print_current_time(type=''):
-    if(type == 'year'):
-        return time.strftime('%Y',time.localtime())
-    elif(type == 'month'):
-        return time.strftime('%Y-%M',time.localtime())
-    elif(type == 'day'):
-        return time.strftime('%Y-%M-%D',time.localtime())
-    elif(type == 'hour'):
-        return time.strftime('%Y-%M-%D %H',time.localtime())
-    elif(type == 'minute'):
-        return time.strftime('%Y-%M-%D %H:%M',time.localtime())
-    elif(type == 'second' or type == ''):
-        return time.strftime('%Y-%M-%D %H:%M',time.localtime())
+
+time_format = {
+    'year': '%Y',
+    'month':'%Y-%m',
+    'day':'%Y-%m-%d',
+    'hour':'%Y-%m-%d %H',
+    'minute':'%Y-%m-%d %H:%M',
+    'second':'%Y-%m-%d %H:%M:%S'
+}
+
+def current_time(format='second'):
+    if(format in time_format):
+        return time.strftime(time_format[format], time.localtime())
+    else:
+        return None
 
 def log_error(message):
     print ('\033[1;31;47m' + message +  '\033[0m')
@@ -21,4 +23,31 @@ def log_info(message):
     print ('\033[8;34;47m' + message + '\033[0m')
 
 def log_warning(message):
-    print ('\033[1;33;47m' + message + '\033[0m')
+    print ('\033[1;33;48m' + message + '\033[0m')
+
+# from time import sleep
+#
+# class a:
+#
+#     b = 3
+#     print ('start1111111')
+#     sleep(5)
+#     print ('start')
+#     def __init__(self):
+#         self.cc = 1
+#         print (self.cc)
+#
+#     @classmethod
+#     def gg(zzz):
+#         a.b = 5
+#         zzz.b = 7
+#     def pri(self):
+#         print ('finally', self.b, a.b)
+#     def hh(self):
+#         self.b = 6
+#
+#
+# g = a()
+# print ('cccc')
+# h = a()
+# j = a()
